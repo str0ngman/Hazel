4011fa24751d9485063e660baefb09165e319f09
Strongman <116357931@qq.com>
2020-01-27 01:14:58 +0800

    GLFW window Created

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,12 +2,13 @@
 #include "Application.h"
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
+#include <GLFW/glfw3.h>
 
 namespace Hazel
 {
 	Application::Application() 
 	{
-	
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 	Application::~Application() 
 	{
@@ -15,17 +16,10 @@ namespace Hazel
 	}
 	void Application::Run() 
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication)) {
-			HZ_TRACE(e);
-		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
-
-		while (true)
-		{
+		while (m_Running) {
+			glClearColor(1, 1, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
 	}
 }
